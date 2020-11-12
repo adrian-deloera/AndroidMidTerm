2407aea51f573f26e472215a0a4b8776b9aaf587
adrian-deloera <adrian.deloera@ou.edu>
2020-11-11 20:39:48 -0600

    Added endpoint connectivity and UI

--- app/src/main/java/com/example/androidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/androidmidterm/MainActivity.kt
@@ -4,14 +4,52 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.example.androidmidterm.R
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Volley uses internet permissions which needs to be enabled in the AndroidManifest.xml
+        val queue = Volley.newRequestQueue(this)
+
+        val url = "https://csfinal-295218.uc.r.appspot.com/"
+
+        // TODO: change this to your url after you have endpoints
+        button.setOnClickListener {
+            val postParameters = JSONObject()
+            try {
+                val size: Int = sizeEntry.text.toString().toInt()
+                val eaf: Int = eafEntry.text.toString().toInt()
+                postParameters.put("size", size)
+                postParameters.put("EAF", eaf)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            // OU calendar returns a Json Array, if your website returns a Json Object then use JsonObjectRequest
+            val request = JsonObjectRequest(Request.Method.POST, url, postParameters,
+                    Response.Listener<JSONObject> { response ->
+                        val event = response
+                        // eventTitle is the id for the textbox in activity_main.xml
+                        modeResponse.text = " Mode: ${event.getString("mode")}"
+                        effortResponse.text = "Effort: ${event.getString("effort")}"
+                        timeResponse.text = "Time: ${event.getString("time")}"
+                        staffResponse.text = "Staff: ${event.getString("staff")}"
+                    },
+                    Response.ErrorListener { staffResponse.text = "That didn't work" })
+            queue.add(request)
+        }
+    }
+}
+
+
+/*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,3 +78,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+ */
